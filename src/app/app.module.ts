dbbe826099d8b51c3daacfbd4db566527f040895
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-09-04 12:39:22 -0300

    Melhorias de apresentação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    SocialLinksComponent
   ],
   imports: [
     BrowserModule,
